9945cc905d7c222521650188db56530959d57dca
Brandon <brandon@dutchsec.com>
2019-04-24 16:15:37 +0200

    clarify

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,10 @@ git commit -m "rebuilding site on `date`, commit ${TRAVIS_COMMIT} and job ${TRAV
 
 # Here too:
 
+# another way
+echo "auth using curl + token"
+curl -u teranos:${GITHUB_AUTH_SECRET} https://api.github.com/teranos
+
 # ${GITHUB_AUTH_SECRET} = <username>:<auth-token>
 git remote add gh-pages https://${GITHUB_AUTH_SECRET}:x-oauth-basic@github.com/teranos/honeytrap-docs.git # > /dev/null 2>&1
 git push --quiet --set-upstream origin gh-pages
